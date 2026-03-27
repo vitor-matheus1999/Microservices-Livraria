5c9ddcec244daf097fe9f55af36bdea7ec59c7ea
bochiin <andre.gomes7110@gmail.com>
2026-03-27 20:50:35 -0300

    fix: remove identity-users

--- identity-users/init.sql
+++
@@ -1,2 +0,0 @@
-create table usuarios;
-create table keycloak;
--- identity-users/src/main/java/com/identity/identity/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.identity.identity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface  UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
-}
